ce3408901941339318e0e1597249682adb29c3bc
enzocazenave <encaze24@hotmail.com>
2022-09-06 16:16:43 -0300

    Add navbar styles

---
+++ mern-chat-app/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+    return (
+        <nav className="Navbar-container">
+            <h1 className="Navbar-container_brand">ChatVZ</h1>
+            <button className="fas fa-bars"></button>
+        </nav>
+    )
+}
--- mern-chat-app/src/pages/MainPage.jsx
+++ mern-chat-app/src/pages/MainPage.jsx
@@ -1,3 +1,5 @@
+import { Navbar } from '../components/';
+
 export const MainPage = () => {
     return (
         <Navbar />
