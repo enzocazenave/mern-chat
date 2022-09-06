5d4b98c0f3e85ce985d574311814275717be9c27
enzocazenave <encaze24@hotmail.com>
2022-09-06 12:44:49 -0300

    Add createAccount functionality

--- mern-chat-api/controllers/auth.js
+++ mern-chat-api/controllers/auth.js
@@ -1,16 +1,36 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async(req, res = response) => {
     
-    const { name, surname, email, password } = req.body;
+    const { email, password } = req.body;
 
     try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo electrónico está en uso'
+            });
+        }
+
+        user = new User(req.body);
+
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        await user.save();
+
+        const token = await generateJWT(user.id, user.name);
+
         res.status(201).json({
             ok: true,
-            name,
-            surname,
-            email,
-            password
+            uid: user.id,
+            name: user.name,
+            token
         });
     } catch(error) {
         console.log(error);
@@ -20,7 +40,6 @@ const createUser = async(req, res = response) => {
             msg: 'Por favor hable con el administrador'
         })
     }
-
 }
 
 module.exports = {
--- mern-chat-api/index.js
+++ mern-chat-api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { dbConnection } = require('./database/config');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+dbConnection();
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${ PORT }`);
