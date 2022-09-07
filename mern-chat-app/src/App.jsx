b85c2104ecd0013ac99ddca4b0d15924d988cd8f
enzocazenave <encaze24@hotmail.com>
2022-09-07 18:00:44 -0300

    Add react redux store

--- mern-chat-app/src/App.jsx
+++ mern-chat-app/src/App.jsx
@@ -1,11 +1,15 @@
 import { AppRouter } from './router';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export const App = () => {
   
   return (
       <BrowserRouter>
-          <AppRouter />
+          <Provider store={ store }>
+              <AppRouter />
+          </Provider>
       </BrowserRouter>
   )
 }
